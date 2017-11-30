514d644f39b629af9c9b9bef4533fd159efc8a05
Milad Xandi <farvahar.milad@hotmail.com>
2017-11-30 17:51:41 +0330

    Bot Full

--- ReturnerBot/Program.cs
+++ ReturnerBot/Program.cs
@@ -13,33 +13,97 @@ namespace ReturnerBot
         static void Main(string[] args)
         {
             var t = Task.Run(() => RunBot());
-            Console.ReadKey();
+            while (Console.ReadKey().KeyChar != '~') ;
         }
         static async Task RunBot()
         {
-            Console.WriteLine("Bot is initializing...");
-            string API = "";
-            var Bot = new TelegramBotClient(API);
-            var Me = await Bot.GetMeAsync();
-            Console.WriteLine($"{Me.Username}");
-            Console.WriteLine($"{Me.Username} HashSet been initialized.");
-            int offset = 0;
-            while (true)
+
+            try
             {
-
-
-                var updates = await Bot.GetUpdatesAsync(offset);
-
-                foreach (var update in updates)
+                Console.WriteLine("Bot is initializing...");
+                if (Properties.Settings.Default.APIToken == "")
                 {
-                    offset = update.Id + 1;
-                    var ChatId = update.Message.Chat.Id;
-                    var Message = update.Message.Text;
-                    await Bot.SendTextMessageAsync(chatId: ChatId, text: Message);
-                    continue;
+                    //Console.WriteLine("Please enter your API: ");
+                    //string rAPI = Console.ReadLine();
+                    //string API = rAPI;
+                    //Properties.Settings.Default.APIToken = rAPI;
+                    var Bot = new TelegramBotClient("343174196:AAGRMRC1HTTdSymyL6pocecjw4XMm8AwuQw");
+                    var Me = await Bot.GetMeAsync();
+                    Console.WriteLine($"{Me.Username}");
+                    Console.WriteLine($"{Me.Username} has been initialized.");
+                    Console.WriteLine("\n");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.Clear();
+                    Console.WriteLine("Press '~' for exit.");
+                    Console.WriteLine($"{Me.Username} is ready to receive message.\n");
+                    int offset = 0;
+                    while (true)
+                    {
+                        var updates = await Bot.GetUpdatesAsync(offset);
+                        foreach (var update in updates)
+                        {
+
+                            offset = update.Id + 1;
+                            var ChatId = update.Message.Chat.Id;
+                            var MessageId = update.Message.MessageId;
+                            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.MessageUpdate)
+                            {
+                                Console.WriteLine($"{update.Message.From.FirstName} {update.Message.From.LastName} ({update.Message.From.Username}): {update.Message.Text}");
+                                if ((update.Message.Text.Contains("http") ||
+                                    update.Message.Text.Contains("https") ||
+                                    update.Message.Text.Contains(".ir") ||
+                                    update.Message.Text.Contains("https") ||
+                                    update.Message.Text.Contains(".com") ||
+                                    update.Message.Text.Contains(".net")))
+                                {
+                                    var Message = update.Message.Text;
+                                    if (Message.Contains("[")|| Message.Contains("]") || Message.Contains("(") || Message.Contains(")") || Message.Contains(":") || Message.Contains(";") || Message.Contains("@") || Message.Contains("*") || Message.Contains("_"))
+                                    {
+                                        await Bot.SendTextMessageAsync(chatId: ChatId, replyToMessageId: MessageId, text: Message, parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+                                        await Bot.SendTextMessageAsync(chatId: ChatId, replyToMessageId: MessageId, text: Message, parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown,disableWebPagePreview:true);
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        await Bot.SendTextMessageAsync(chatId: ChatId, replyToMessageId: MessageId, text: @"لطفا برای استفاده از امکانات توسعه دهندگان از این الگو استفاده کنید:
+*bold text*
+_italic text_
+[inline URL](http://www.example.com/)
+[inline mention of a user](tg://user?id=123456789)
+`inline fixed-width code`
+```block_language
+pre - formatted fixed-width code block
+```");
+                                        continue;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.Write("You: ");
+                                    string Message = Console.ReadLine();
+                                    //var Message = update.Message.Text;
+                                    await Bot.SendTextMessageAsync(chatId: ChatId, replyToMessageId: MessageId, text: Message);
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                await Bot.SendTextMessageAsync(chatId: ChatId, text: "ما تنها قادر به پاسخ به پیام های متنی شما هستیم.");
+                                continue;
+                            }
+                        }
+                    }
                 }
             }
-            
+            catch (Telegram.Bot.Exceptions.ApiRequestException error)
+            {
+
+                Console.WriteLine($"{error.Message} , we`ll reset for you in 5 seconds.");
+                System.Threading.Thread.Sleep(5000);
+                Console.Clear();
+                Properties.Settings.Default.APIToken = "";
+                var t = Task.Run(() => RunBot());
+            }
         }
     }
 }
--- ReturnerBot/Properties/AssemblyInfo.cs
+++ ReturnerBot/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,12 +7,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ReturnerBot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bot Chatter.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("eastCloud LLC.")]
 [assembly: AssemblyProduct("ReturnerBot")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Milad Zandi , 2017")]
+[assembly: AssemblyTrademark("Milad Zandi")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("fa-IR")]
+
